26e6948c423414f6c9a67834dca28eaa7bda5f18
Anton Bil <anton.bil@hz.nl>
2016-09-29 19:51:31 +0000

    added some files

---
+++ alternative/electronic/addlink.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>Add link</title>
+ </head>
+ <body>
+ <?php
+$url=$_GET['url'];
+$artist=$_GET['artist'];
+$artistsort=$_GET['artistsort'];
+$album=$_GET['album'];
+$out="\n".$url.",".$artist.",".$artistsort.",".$album;
+file_put_contents('links.txt', $out, FILE_APPEND);
+?>
+ </body>
+</html>
---
+++ folk/addlink.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>Add link</title>
+ </head>
+ <body>
+ <?php
+include '../bin/addlink.php';
+?>
+ </body>
+</html>
