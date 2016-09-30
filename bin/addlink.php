32e0562585d04910be7810b4fca6292d4e18ee8d
anton <anton.bil@hz.nl>
2016-09-30 22:08:08 +0200

    change file

--- bin/addlink.php
+++ bin/addlink.php
@@ -1,8 +1,10 @@
  <?php
 $url=$_GET['url'];
 $artist=$_GET['artist'];
+$artist = str_replace(",", " ", $artist);
 $artistsort=$_GET['artistsort'];
 $album=$_GET['album'];
+$album = str_replace(",", " ", $album);
 $pictureurl=$_GET['pictureurl'];
 $out="\n".$url.",".$artist.",".$artistsort.",".$album.",".$pictureurl;
 file_put_contents('links.txt', $out, FILE_APPEND);
