1bf34d3b38d53aa9f45277bd8eb243b0d1cf6885
anton <anton.bil@hz.nl>
2016-10-02 11:25:58 +0200

    change file

--- bin/addlink.php
+++ bin/addlink.php
@@ -8,7 +8,10 @@ $album = str_replace(",", " ", $album);
 $pictureurl=$_GET['pictureurl'];
 //https://api.spotify.com/v1/albums/{id}
 if ((strlen ($artist)==0) ||(strlen ($album)==0) || (strlen ($pictureurl)==0)){
-  $response = file_get_contents('https://api.spotify.com/v1/albums/'.$url);
+  $myurl=str_replace ( "spotify:album:","",$url);
+  $spotifyUri='https://api.spotify.com/v1/albums/'.$myurl;
+  $response = file_get_contents($spotifyUri);
+  echo "Address: $spotifyUri<br/>";
   $response = json_decode($response);
   var_dump($response);
 } else {
