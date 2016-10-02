70ab8fc40eadce3e1ec0b72330a3d619322278d2
anton <anton.bil@hz.nl>
2016-10-02 11:34:44 +0200

    change file

--- bin/addlink.php
+++ bin/addlink.php
@@ -13,7 +13,7 @@ if ((strlen ($artist)==0) ||(strlen ($album)==0) || (strlen ($pictureurl)==0)){
   $response = file_get_contents($spotifyUri);
   echo "Address: $spotifyUri<br/>";
   $response = json_decode($response);
-  $name=$reponse["artists"][0]["name"];
+  $name=$reponse["artists"][0].name;
   echo "Name: $name<br/>";
   var_dump($response);
 } else {
