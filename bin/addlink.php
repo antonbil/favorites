490b860f2c5ad09d9dfa21b01eaf2b35835e48c0
anton <anton.bil@hz.nl>
2016-10-02 13:20:15 +0200

    change file

--- bin/addlink.php
+++ bin/addlink.php
@@ -1,4 +1,23 @@
  <?php
+ function display_form(){
+    echo '
+    <form id="form1" name="form1" method="get" action="addlink.php">
+    Add album to list<p>
+    <label>Spotify-Album-Uri:
+    <input type="text" name="url" id="url" />
+    </label><br/>
+    <label>Sort-item
+    <input type="text" name="artistsort" id="artistsort" />
+    </label>
+
+    <p>
+    <label>Submit
+    <input type="submit" name="submit" id="submit" value="Submit" />
+    </label>
+    </p>
+    </form>
+    ';
+}
 $url=$_GET['url'];
 $artist=$_GET['artist'];
 $artist = str_replace(",", " ", $artist);
@@ -7,23 +26,7 @@ $album=$_GET['album'];
 $album = str_replace(",", " ", $album);
 $pictureurl=$_GET['pictureurl'];
 //https://api.spotify.com/v1/albums/{id}
-if (strlen ($url)==0){echo '
- <form id="form1" name="form1" method="get" action="addlink.php">
- Add album to list<p>
- <label>Spotify-Album-Uri:
- <input type="text" name="url" id="url" />
- </label><br/>
- <label>Sort-item
- <input type="text" name="artistsort" id="artistsort" />
- </label>
-
- <p>
- <label>Submit
- <input type="submit" name="submit" id="submit" value="Submit" />
- </label>
- </p>
- </form>
-';
+if (strlen ($url)==0){
 } else {
     if ((strlen ($artist)==0) ||(strlen ($album)==0) || (strlen ($pictureurl)==0)){
     $myurl=str_replace ( "spotify:album:","",$url);
@@ -42,8 +45,11 @@ if (strlen ($url)==0){echo '
     echo $out."<br/>";
     file_put_contents('links.txt', $out, FILE_APPEND);
     } else {
-    $out="\n".$url.",".$artist.",".$artistsort.",".$album.",".$pictureurl;
-    file_put_contents('links.txt', $out, FILE_APPEND);
+        echo $out."<br/>";
+        $out="\n".$url.",".$artist.",".$artistsort.",".$album.",".$pictureurl;
+        file_put_contents('links.txt', $out, FILE_APPEND);
     }
 }
+  display_form();
+
 ?>
